fcd9a743692f012da462336a4bbd6eb5b23d7e82
bmorris213 <thisisnotalreadyused@gmail.com>
2024-06-22 06:15:47 -0600

    Added getURLsFromHTML and normalizeURL, as well as tests for these functions, to crawl.js

--- crawl.js
+++ crawl.js
@@ -1,5 +1,119 @@
 function normalizeURL(urlString) {
-    
+    try {
+        //step 1: break up url string into component objects
+        const parsedURL = new URL(urlString);
+
+        //step 2: remove empty pathnames
+        if (parsedURL.pathname === '/') {
+            parsedURL.pathname = '';
+        }
+
+        //step 2.5: remove trailing / from domain
+        if (parsedURL.hostname.slice(-1) === '/') {
+            parsedURL.hostname = parsedURL.hostname.slice(0, -1);
+        }
+
+        //step 3: remove default ports
+        if (parsedURL.protocol === 'http:') {
+            if (parsedURL.port === '80') {
+                parsedURL.port = '';
+            }
+        }
+        else if (parsedURL.protocol === 'https:') {
+            if (parsedURL.port === '443') {
+                parsedURL.port = '';
+            }
+        }
+
+        //step 4: remove default path file object
+        if (parsedURL.pathname === '/index.html') {
+            parsedURL.pathname = '';
+        }
+
+        //step 5: remove default 'www' subdomain
+        if (parsedURL.hostname.substring(0, 4) === 'www.') {
+            parsedURL.hostname = parsedURL.hostname.substring(4);
+        }
+
+        //step 6: sort query parameters alphabetically
+        const searchParams = new URLSearchParams(parsedURL.search);
+
+        // Convert the search parameters to an array and sort them
+        const sortedParams = [...searchParams.entries()].sort(([key1], [key2]) => {
+            return key1.localeCompare(key2);
+        });
+
+        //Create new search parameter object
+        const newSearchParams = new URLSearchParams();
+        sortedParams.forEach(([key, value]) => {
+            newSearchParams.append(key, value);
+        });
+
+        // Set the sorted search params back to the URL object
+        parsedURL.search = newSearchParams.toString();
+
+        //step 7: decode each component to remove existing encodings
+        parsedURL.pathname = decodeURIComponent(parsedURL.pathname);
+        parsedURL.search = decodeURIComponent(parsedURL.search);
+
+        //step 8: re-encode almost everything in pathname and search
+        parsedURL.pathname = encodeURIComponent(parsedURL.pathname);
+        parsedURL.search = encodeURIComponent(parsedURL.search);
+
+        //step 9: return decoding of needed search parameters
+        parsedURL.search = parsedURL.search.replace(/%25/g, '%').replace(/%3F/g, '?').replace(/%3D/g, '=').replace(/%26/g, '&').replace(/%2F/g, '');
+        
+        parsedURL.pathname = parsedURL.pathname.replace(/%25/g, '%').replace(/%2F/g, '');
+
+        //step 10: fix parsing of empty strings
+        let parsedPort = ''
+        if (parsedURL.port !== '') {
+            parsedPort = `:${parsedURL.port}`;
+        }
+
+        let parsedPath = ''
+        if (parsedURL.pathname !== '/') {
+            parsedPath = parsedURL.pathname;
+        }
+
+        //step 10.5: fix duplicate ? results
+        let parsedSearch = ''
+        if (parsedURL.search !== '') {
+            parsedSearch = `?${parsedURL.search.replace(/\?/g, '')}`
+        }
+
+        //step 11: reencode url string
+        const normalizedURL = `${parsedURL.hostname}${parsedPort}${parsedPath}${parsedSearch}`;
+
+        return normalizedURL;
+    } catch (err) {
+        throw new Error('Invalid URL');
+    }
 }
 
-export { normalizeURL };
+function getURLsFromHTML(htmlBody, baseURL) {
+    //relative paths start with '/'
+    const links = []
+    for (const line of htmlBody.split('\n')) {
+        if (line.includes('<a')) {
+            let regLine = line.match(/href=\"(.*)\">/g).pop()
+            regLine = regLine.replace(/href=\"/g, '').replace(/\">/g, '')
+            links.push(regLine)
+        }
+    }
+
+    //make all relative links into absolute links
+    const cleanLinks = []
+    for (let link of links) {
+        if (link[0] === '/') {
+            cleanLinks.push(`${baseURL}${link}`);
+        }
+        else {
+            cleanLinks.push(link)
+        }
+    }
+
+    return cleanLinks
+}
+
+export { normalizeURL, getURLsFromHTML };
--- crawl.test.js
+++ crawl.test.js
@@ -1,2 +1,121 @@
-import { test, expect } from "@jest/globals";
-import { normalizeURL } from "./crawl.js";
+import { test, expect, describe } from "@jest/globals";
+import { normalizeURL, getURLsFromHTML } from "./crawl.js";
+
+describe('testing normalizeURL function edge cases', () => {
+
+    let resultUrl = 'google.com';
+    let testUrl = 'https://google.com/';
+
+    test('test default value', () => {
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('remove trailing slash for google.com', () => {
+        testUrl = 'https://google.com';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('change protocol for google.com', () => {
+        testUrl = 'http://google.com/';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('ensure case insensitivity in domain name', () => {
+        testUrl = 'https://Google.com/';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+        testUrl = 'https://GOOGLE.com/';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('test default subdomain of www', () => {
+        testUrl = 'https://www.google.com/';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('test default landing file name', () => {
+        testUrl = 'https://www.google.com/index.html';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('test default port values for different protocols', () => {
+        testUrl = 'https://google.com:443/';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+        testUrl = 'http://google.com:80/';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('verify non-default port values are kept', () => {
+        resultUrl = 'google.com:443';
+        testUrl = 'http://google.com:443/';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+
+        resultUrl = 'google.com:80';
+        testUrl = 'https://google.com:80/';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+
+        resultUrl = 'google.com:400';
+        testUrl = 'https://google.com:400/';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('changing order of query parameters', () => {
+        resultUrl = 'google.com/search?client=firefox&q=cats';
+        testUrl = 'https://google.com/search?q=cats&client=firefox';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('test encoding spaces', () => {
+        resultUrl = 'google.com/test%20space';
+        testUrl = 'https://google.com/test space';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('test encoding invalid characters', () => {
+        resultUrl = 'google.com/test%40atsign';
+        testUrl = 'https://google.com/test@atsign';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('test double encoding issues', () => {
+        resultUrl = 'google.com/foo%20bar';
+        testUrl = 'https://google.com/foo%2520bar';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('test already encoded strings', () => {
+        resultUrl = 'google.com/foo%20bar';
+        testUrl = 'https://google.com/foo%20bar';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+
+    test('test fragment identifiers are stripped', () => {
+        resultUrl = 'google.com/page';
+        testUrl = 'https://google.com/page#section';
+        expect(normalizeURL(testUrl)).toBe(resultUrl);
+    });
+});
+
+describe('testing getURLsFromHTML function edge cases', () => {
+    const testBase = 'https://example.com';
+    let testHTML = '<html>\n\t<body>\n\t\t';
+    testHTML += '<h1>TITLE of the HTML test doc</h1>\n\t\t';
+    testHTML += '<p>This is an example text paragraph...</p>\n\t\t';
+    testHTML += '<ol>\n\t\t\t<li><a href=\"https://example.com/path\"><span>Go to Home</span></a></li>\n\t\t\t';
+    testHTML += '<li><a href=\"/shop\"><span>Go to Shop</span></a></li>\n\t\t\t';
+    testHTML += '<li><a href=\"/path/blank\"><span>Go to Blank</span></a></li>\n\t\t\t';
+    testHTML += '<li><a href=\"/shop?source=blank\"><span>Go to Shop from Blank</span></a></li>\n\t\t\t';
+    testHTML += '<li><a href=\"https://google.com/\"><span>Go to Google</span></a></li>';
+    testHTML += '\n\t\t</ol>\n\t</body>\n</html>';
+
+    const urlList = [
+        'https://example.com/path',
+        'https://example.com/shop',
+        'https://example.com/path/blank',
+        'https://example.com/shop?source=blank',
+        'https://google.com/'
+    ];
+
+    test('asserting extraction works', () => {
+        expect(getURLsFromHTML(testHTML, testBase)).toStrictEqual(urlList);
+    });
+});
